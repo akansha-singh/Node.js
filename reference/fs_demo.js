a30b1f2fe7c80b94d8e765b3ee9dcc5e6c036640
akansha-singh <singhakansha281@gmail.com>
2020-04-05 15:44:55 +0530

    fs.rename

--- reference/fs_demo.js
+++ reference/fs_demo.js
@@ -20,9 +20,15 @@ fs.writeFile(path.join(__dirname, '/test', 'hello.txt'), 'Hello World!', err => 
 });
 */
 
-// Read file
+/*// Read file
 fs.readFile(path.join(__dirname, '/test', 'hello.txt'), 'utf8', (err, data) => {
     if (err) throw err;
     console.log(data);
 });
+*/
 
+// Reaname file
+fs.rename(path.join(__dirname, '/test', 'hello.txt'), path.join(__dirname, '/test', 'helloworld.txt'), (err) => {
+    if (err) throw err;
+    console.log('File renamed...');
+});
