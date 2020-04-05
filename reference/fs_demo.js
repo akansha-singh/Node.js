4f97091952dce46db01893bd302748201ae80f14
akansha-singh <singhakansha281@gmail.com>
2020-04-05 15:35:04 +0530

    fs.readFile

--- reference/fs_demo.js
+++ reference/fs_demo.js
@@ -8,8 +8,21 @@ fs.mkdir(path.join(__dirname, '/test'), {}, err => {
 });
 */
 
-// Create and write to file
+
+/*// Create and write to file
 fs.writeFile(path.join(__dirname, '/test', 'hello.txt'), 'Hello World!', err => {
     if (err) throw err;
     console.log('File written to...');
+    fs.appendFile(path.join(__dirname, '/test', 'hello.txt'), 'I love node.js', err => {
+        if (err) throw err;
+        console.log('File written to...');
+    });
 });
+*/
+
+// Read file
+fs.readFile(path.join(__dirname, '/test', 'hello.txt'), 'utf8', (err, data) => {
+    if (err) throw err;
+    console.log(data);
+});
+
