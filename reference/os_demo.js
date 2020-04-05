2a485e4b8de22f18878ab7ce75abb4d19be36a05
akansha-singh <singhakansha281@gmail.com>
2020-04-05 16:00:47 +0530

    os

---
+++ reference/os_demo.js
@@ -0,0 +1,22 @@
+const os = require('os');
+
+// Platform
+console.log(os.platform());
+
+//CPU Arch
+console.log(os.arch());
+
+// Free memory
+console.log(os.freemem());
+
+// Total memory
+console.log(os.totalmem());
+
+//CPU Core Info
+console.log(os.cpus());
+
+//Home dir
+console.log(os.homedir());
+
+// Uptime
+console.log(os.uptime());
