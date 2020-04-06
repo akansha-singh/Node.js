47668b8b9762503a5582bf304b4feff123a7756c
akansha-singh <singhakansha281@gmail.com>
2020-04-06 16:43:33 +0530

    Server created

--- index.js
+++ index.js
@@ -1,10 +1,31 @@
-const Logger = require('./logger');
+const http = require('http');
+const path = require('path');
+const fs = require('fs');
+
+const server = http.createServer((req, res) => {
+    //console.log(req.url);
+    if (req.url === '/') {
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.end('<h1>Home</h1>');
+    }
+});
+
+const PORT = process.env.PORT || 5000;
+
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+
+
+
+/*const Logger = require('./logger');
 
 const logger = new Logger();
 
 logger.on('message', data => console.log('Called Listener', data));
 
 logger.log('Hello World');
+*/
+
 
 /*const Person = require('./person');
 
