27a8c73de089693cc51d5b81464a92f1f583351e
akansha-singh <singhakansha281@gmail.com>
2020-04-06 12:06:35 +0530

    logger

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
-const Person = require('./person');
+const Logger = require('./logger');
+
+const logger = new Logger();
+
+logger.on('message', data => console.log('Called Listener', data));
+
+logger.log('Hello World');
+
+/*const Person = require('./person');
 
 const person1 = new Person('John Doe', 30);
 
 person1.greeting();
+*/
